db7c12d79d413cf06e8055defcc5be1246e88cd3
kevinrrdev <kevinreyesrojas3@gmail.com>
2018-06-07 02:05:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kevin/mvp_example/CalculatorView.java
@@ -0,0 +1,13 @@
+package com.example.kevin.mvp_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_view);
+    }
+}
